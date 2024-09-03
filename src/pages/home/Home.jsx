b8effcdad934be84980c78c51f65a9288e4f993e
mhammad100 <hammadmansha100@gmail.com>
2024-09-03 16:47:09 +0500

    file import fixed

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Slider from './components/slider/Slider'
 import Banners from './components/banners/banners'
 import Product_card from './components/summer-sale/SummerSale'
-import BestBrand from './components/bestBrand/bestBrand'
+import BestBrand from './components/bestBrand/BestBrand'
 function Home() {
     return (
         <>
