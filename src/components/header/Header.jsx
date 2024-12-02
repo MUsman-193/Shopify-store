99a4dda83d334fb47640760b77bdeb429297c328
mhammad100 <hammadmansha100@gmail.com>
2024-12-02 12:34:15 +0500

    design changes

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,78 +1,88 @@
-import React from "react";
+import React,{useState} from "react";
 import header_logo from "../../assets/image/Home_images/logo/brand_logo.png";
-import { BsSearch } from "react-icons/bs";
-import { AiOutlineShoppingCart } from "react-icons/ai";
-import { HiLocationMarker } from "react-icons/hi";
-import { BsFillPersonFill } from "react-icons/bs";
-import { AiOutlineBars } from "react-icons/ai";
+import { CiSearch } from "react-icons/ci";
+import { BsPersonFill } from "react-icons/bs";
+import { FiShoppingCart } from "react-icons/fi";
+import { HiMiniBars3 } from "react-icons/hi2";
+
 import "./Header.css";
+
+const data = [
+  {
+    name: "MEN",
+    path: "/",
+  },
+  {
+    name: "WOMEN",
+    path: "/",
+  },
+  {
+    name: "KIDS",
+    path: "/",
+  },
+  {
+    name: "HOME & LIVING",
+    path: "/",
+  },
+  {
+    name: "BEAUTY",
+    path: "/",
+  },
+];
+
+const NavigationBar = () => {
+  const handlePrevent = (e) => {
+    e.stopPropagation();
+  };
+
+  return (
+    <nav onClick={handlePrevent}>
+      <ul>
+        {data.map((el, index) => (
+          <li key={index}>
+            <a  href={el.path}> 
+              {el.name}
+              </a>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+
 export default function Header() {
+  const [showSidebar, setShowSidebar] = useState(false);
+
+  const handleSideBarToggle = () => {
+      setShowSidebar(!showSidebar);
+  }
   return (
     <>
-      <header>
-        <div className="header_slider">
-          <marquee className="header_slider_txt">
-            Big Offer Dhamaka on F&B, Grocery, Beauty Care, Health and Wellness,
-            Home and Kitchen, Fashion & Electronics. You Can Check the Discounts
-            on Checkout Page.
-          </marquee>
+      <marquee className="header_txt_slider">
+        Big Offer Dhamaka on F&B, Grocery, Beauty Care, Health and Wellness,
+        Home and Kitchen, Fashion & Electronics. You Can Check the Discounts on
+        Checkout Page.
+      </marquee>
+
+      <header className="header">
+        <div className="header_logo">
+          <img src={header_logo} alt="" />
         </div>
-        <div className="header">
-          <div className="logo_nav_container">
-            <div className="header_nav_logo">
-              <AiOutlineBars />
-            </div>
-            <div className="header_logo">
-              <img src={header_logo} alt="" />
-            </div>
-            <nav className="nav_bar">
-              <ul>
-                <li>Store Near Me</li>
-                <li>Product Near Me</li>
-                <li>Fashion</li>
-                <li>Beauty & Personal Care</li>
-                <li>Home Decor</li>
-                <li>Electronics</li>
-              </ul>
-            </nav>
-          </div>
-          <div className="header_search_container">
-            <div className="header_search">
-              <div className="header_search_icon">
-                <BsSearch />
-              </div>
-              <div className="header_input">
-                <input type="text" placeholder="Search" />
-              </div>
-            </div>
-            <nav className="header_icons">
-              <ul>
-                <li>
-                  {" "}
-                  <AiOutlineShoppingCart />{" "}
-                </li>
-                <li>
-                  {" "}
-                  <BsFillPersonFill />{" "}
-                </li>
-                <li>
-                  {" "}
-                  <HiLocationMarker />{" "}
-                </li>
-              </ul>
-            </nav>
-          </div>
-        </div>
-        {/* -------------------------- */}
-        <div className="header_input_container_2">
-          <div className="header_search_2">
-            <div className="header_search_icon">
-              <BsSearch />
-            </div>
-            <div className="header_input">
-              <input type="text" placeholder="Search" />
-            </div>
-          </div>
+        <nav>
+          <ul>
+            <li>MEN</li>
+            <li>WOMEN</li>
+            <li>KIDS</li>
+            <li>HOME & LIVING</li>
+            <li>BEAUTY</li>
+          </ul>
+        </nav>
+        <div className="header_icons">
+          <CiSearch />
+          <BsPersonFill />
+          <FiShoppingCart />
+          <HiMiniBars3/>
         </div>
       </header>
     </>
